9ece5b805ec54a8bb16ad782a2198d48fddafdeb
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-05-18 17:26:55 +0100

    updated to version 0.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clojure-encog "0.3.0-SNAPSHOT"
-  :description "Native Clojure wrapper for Encog(v3) Machine-Learning framework."
+(defproject clojure-encog "0.4.0-SNAPSHOT"
+  :description "Thin Clojure wrapper for Encog(v3) Machine-Learning framework."
   :url "http://github.com/jimpil/clojure-encog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,5 +9,5 @@
   :jvm-opts ["-Xmx1g"] 
   ;:javac-options {:classpath "target/dependency/encog-core-3.1.0.jar" :destdir "target/classes"}
   ;:java-source-path "src/java"
-  ;:main       clojure-encog.examples
+  ;:main     clojure-encog.examples
   )
